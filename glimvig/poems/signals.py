cf201c54a2eddd872486d1375fe2b195652f27b9
Ait <keniusmals@gmail.com>
2026-08-28 11:30:45 +0300

    fix(projn): fix some issues

--- glimvig/poems/signals.py
+++ glimvig/poems/signals.py
@@ -6,6 +6,6 @@ from .models import Rating, Poem
 @receiver([post_save, post_delete], sender=Rating)
 def update_poem_average_rating(sender, instance, **kwargs):
     poem = instance.poem
-    avg = Rating.object.filter(poem=poem).aggregate(Avg('value'))['value__avg']
+    avg = Rating.objects.filter(poem=poem).aggregate(Avg('value'))['value__avg']
     poem.average_rating = avg if avg is not None else 0
     poem.save(update_fields=['average_rating'])
